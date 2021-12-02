31951784ae294ad5d431419a4e549389c87f62ec
MangBao <mangbao1301@gmail.com>
2021-12-02 15:31:53 +0700

    Sửa tên component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/Home/Banner';
-import Content from './component/Home/Content';
+import Navbar from './component/Home/Navs';
+import Header from './component/Home/Header';
 import './css/cssapp.css'
 // import './css/animation.css'
-// import Header from './component/Header';
-// import Banner from './component/Banner';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Content />
+      <Header />
+      
     </div>
   );
 }
